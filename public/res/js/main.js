e246df2010bf4db4f26e66b973f1a9b4dcbed130
Pascal Wittler <git@pascalwittler.de>
2022-10-23 11:57:13 +0200

    request permission for notifications + add example

--- public/res/js/main.js
+++ public/res/js/main.js
@@ -1,3 +1,16 @@
+if ('Notification' in window) {
+  if (Notification.permission !== 'granted') {
+    Notification.requestPermission();
+  }
+
+  setTimeout(() => {
+    new Notification('Heute schon geblubbt?', {
+      body: 'Maximilian hat schon zweimal geblubbt. Und du?',
+      icon: '/res/img/favicon/blubb.today-32x32.png',
+    });
+  }, 1000);
+}
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('res/js/service-worker.js');
 }
