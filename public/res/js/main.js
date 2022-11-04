866363e64228a2a7c36a73b82353dda5168ff09c
Pascal Wittler <git@pascalwittler.de>
2022-11-05 00:21:46 +0100

    add functionality for localStorage + progress bar

--- public/res/js/main.js
+++ public/res/js/main.js
@@ -1,3 +1,7 @@
+if (!localStorage.getItem(new Date().toISOString().split('T')[0])) {
+  localStorage.setItem(new Date().toISOString().split('T')[0], '0');
+}
+
 if ('Notification' in window) {
   if (Notification.permission !== 'granted') {
     Notification.requestPermission();
@@ -17,6 +21,12 @@ if ('serviceWorker' in navigator) {
 
 const addMenuOpener = document.querySelector('.add-menu-opener');
 const addMenu = document.querySelector('.add-menu');
+
+const blubbStatusProgress = document.querySelector('progress');
+const blubbStatusLabel = document.querySelector('.current');
+
+blubbStatusProgress.value = parseInt(localStorage.getItem(new Date().toISOString().split('T')[0]));
+blubbStatusLabel.innerHTML = localStorage.getItem(new Date().toISOString().split('T')[0]);
 
 addMenuOpener.addEventListener('click', () => {
   addMenu.showModal();
