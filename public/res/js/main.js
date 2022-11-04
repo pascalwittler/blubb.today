6aa7578da2fd45c6b3d34f8a316f9ca3652f5497
Pascal Wittler <git@pascalwittler.de>
2022-11-05 00:20:28 +0100

    add dialog (popup) for adding progress

--- public/res/js/main.js
+++ public/res/js/main.js
@@ -14,3 +14,10 @@ if ('Notification' in window) {
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('res/js/service-worker.js');
 }
+
+const addMenuOpener = document.querySelector('.add-menu-opener');
+const addMenu = document.querySelector('.add-menu');
+
+addMenuOpener.addEventListener('click', () => {
+  addMenu.showModal();
+});
